a2b5066841d8775b32ece8df9be3a11eb9a4b3f8
joris <joris@f972ef24-6785-6244-9f4f-f595f474ce1c>
2013-12-06 08:10:30 +0000

    [TASK] DmsObjectNodeRefPeer:: added static function that retrieves the ref by object

--- lib/model/DmsObjectNodeRefPeer.php
+++ lib/model/DmsObjectNodeRefPeer.php
@@ -9,4 +9,17 @@
  */ 
 class DmsObjectNodeRefPeer extends BaseDmsObjectNodeRefPeer
 {
+  /**
+   * geeft object node ref terug op basis van
+   * @param mixed $object
+   * @return DmsObjectNodeRef
+   */
+  public static function retrieveByObject($object)
+  {
+    $c = new Criteria();
+    $c->add(self::OBJECT_CLASS, get_class($object));
+    $c->add(self::OBJECT_ID, $object->getId());
+
+    return self::doSelectOne($c);
+  }
 }
