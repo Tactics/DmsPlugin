f79d241a15ace0cae19a1b160a5cf87dd0f9658a
Joris Hontele <joris@tactics.be>
2014-02-17 15:13:26 +0100

    Added DmsAspectPeer::retrieveBySysName($sys_name)

--- lib/model/DmsAspectPeer.php
+++ lib/model/DmsAspectPeer.php
@@ -22,4 +22,20 @@ class DmsAspectPeer extends BaseDmsAspectPeer
 
     return $options;
   }
+
+  /**
+   * Geeft het aspect met de gegeven system_name terug
+   * 
+   * @param string $sys_name
+   * @return \DmsAspect
+   */
+  public static function retrieveBySystemName($sys_name)
+  {
+    $c = new Criteria();
+    $c->add(self::SYSTEM_NAME, $sys_name);
+
+    return self::doSelectOne($c);
+  }
+
+
 }
