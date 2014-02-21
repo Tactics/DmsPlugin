f050677590ddb8b4724732710a8a9c659f07776d
Joris Hontelé <joris@tactics.be>
2014-02-21 02:32:27 -0800

    [ENHANCE] redirect_url option added in show van node

--- modules/ttDmsBrowser/actions/actions.class.php
+++ modules/ttDmsBrowser/actions/actions.class.php
@@ -325,6 +325,8 @@ class ttDmsBrowserActions extends sfActions
     $c = new Criteria();
     $c->add(DmsAspectPeer::ID, $aspect_ids, Criteria::NOT_IN);
     $this->aspects = DmsAspectPeer::doSelect($c);
+
+    $this->redirect_url = $this->getRequestParameter('redirect_url', 'ttDmsBrowser/list');
   }
 
   /**
@@ -372,7 +374,7 @@ class ttDmsBrowserActions extends sfActions
       }
     }
 
-    $this->redirect('ttDmsBrowser/list');
+    $this->redirect($this->getRequestParameter('redirect_url', 'ttDmsBrowser/list'));
   }
 
   /**
